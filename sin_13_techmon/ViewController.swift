45137172fdae73195c5b05150d94906fbd63c934
sin <yuho.shin@gmail.com>
2022-05-12 10:59:08 +0900

    Initial Commit

---
+++ sin_13_techmon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sin_13_techmon
+//
+//  Created by 菊地英治 on 2022/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
